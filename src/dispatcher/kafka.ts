c41499ce896c80433a284b537291b3b5eda5a08b
NV4RE <tosbodesz@gmail.com>
2019-07-31 14:08:31 +0700

    Add default options

--- src/dispatcher/kafka.ts
+++ src/dispatcher/kafka.ts
@@ -2,26 +2,37 @@ import * as KafkaClient from 'node-rdkafka';
 import { IDispatcher } from './';
 import * as Task from '../task';
 
+// https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
 const DEFAULT_PRODUCER_CONF = {
-  'client.id': 'kafka',
-  'metadata.broker.list': 'localhost:9092',
-  'compression.codec': 'gzip',
-  'retry.backoff.ms': 200,
-  'message.send.max.retries': 10,
+  'client.id': 'SAGA_PM',
+  'bootstrap.servers': 'localhost:9092',
+  'compression.type': 'snappy',
+  'retry.backoff.ms': 100,
+  'enable.idempotence': true,
+  'message.send.max.retries': Number.MAX_SAFE_INTEGER,
   'socket.keepalive.enable': true,
-  'queue.buffering.max.messages': 100000,
-  'queue.buffering.max.ms': 1000,
-  'batch.num.messages': 1000000,
+  'queue.buffering.max.messages': 10000,
+  'queue.buffering.max.ms': 100,
+  'batch.num.messages': 100000,
+  'delivery.report.only.error': true,
   dr_cb: true,
+};
+
+const DEFAULT_PRODUCER_TOPIC_CONFIG = {
+  acks: -1,
+  'compression.type': 'snappy',
 };
 
 export class KafkaDispatcher implements IDispatcher {
   private client: KafkaClient.Producer;
-  constructor(overidePruducerConf: { [key: string]: any }) {
-    this.client = new KafkaClient.Producer({
-      ...overidePruducerConf,
-      ...DEFAULT_PRODUCER_CONF,
-    });
+  constructor(
+    overidePruducerConf: { [key: string]: any },
+    overidePruducerTopicConf: { [key: string]: any },
+  ) {
+    this.client = new KafkaClient.Producer(
+      { ...overidePruducerConf, ...DEFAULT_PRODUCER_CONF },
+      { ...overidePruducerTopicConf, ...DEFAULT_PRODUCER_TOPIC_CONFIG },
+    );
     this.client.connect();
   }
   dispatch(taskName: string, task: Task.Task) {
