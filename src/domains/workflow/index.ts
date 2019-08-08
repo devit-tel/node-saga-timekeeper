b96faeb6415ba990add43de80b996df7e1a3dff1
NV4RE <tosbodesz@gmail.com>
2019-08-08 15:48:35 +0700

    Add a domain

---
+++ src/domains/workflow/index.ts
@@ -0,0 +1,14 @@
+import { workflowDefinitionStore } from '../../store';
+import { NotFound } from '../../errors';
+
+export const startWorkflow = async (
+  workflowName: string,
+  workflowRef: number,
+): Promise<string> => {
+  const workflowData = await workflowDefinitionStore.getValue(
+    `${workflowName}.${workflowRef}`,
+  );
+  if (!workflowData) {
+    throw new NotFound('Workflow not found', 'WORKFLOW_NOT_FOUND');
+  }
+};
